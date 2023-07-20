6cc1e7942883d099ab6cbd68d74ed27c4cb9ea07
Pooja Patil <poojagp.xworkz@gmail.com>
2023-07-21 00:10:01 +0530

    Chaining-Constructor

---
+++ Chaining_Constructor/MallKiller.java
@@ -0,0 +1,14 @@
+class MallKiller{
+	
+	public static void main(String[] args)
+	{
+		System.out.println("invoking Mall");
+		
+		Mall mall=new Mall("Orion Mall");
+		
+		Mall mall1=new Mall("Orion Mall","Shopping Mall","Banglore",37000);
+		
+		Mall mall2=new Mall("Orion Mall","Shopping Mall","Banglore",37000,"Food Court","Vertical Malls",2012);
+		
+	}
+}
